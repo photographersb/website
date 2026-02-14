4dd5cfc3b4122bfe60c817e6209b2d38a0656df9
Mahidul Islam Nakib <bideshgomon@gmail.com>
2026-02-14 16:45:15 +0600

    remove: exclude _CLEANUP_FILES from git tracking

--- _CLEANUP_FILES/run_migration.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/photographersb/htdocs/photographersb.com/public
-php artisan migrate --force 2>&1 | tee /tmp/migration_output.txt
-echo "Exit code: $?" >> /tmp/migration_output.txt
